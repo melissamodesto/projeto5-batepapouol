801b28411521abe37ec2e8c05d6fd9fd6414cc56
melissamodesto <mbm.modesto@gmail.com>
2022-06-25 11:43:41 -0300

    show messages

--- script.js
+++ script.js
@@ -1,7 +1,90 @@
-const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants');
-promise.then(processResponse);
+let participants;
+let message;
 
-function processResponse(response) {
-    let resposta = response.data;
-    console.log(resposta);
+searchMessager();
+choiceUserName();
+
+//Carregar mensagens da API e renderizar na tela
+function searchMessager() {
+    const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    promise.then(processMessager);
 }
+
+function processMessager(response) {
+    message = response.data;
+    console.log(message);
+    renderizarMensagem();
+}
+
+function renderizarMensagem() {
+    const container = document.querySelector('.container');
+    container.scrollIntoView(block = 'end', behavior = 'smooth');
+
+    for (let i = 0; i < message.length; i++) {
+
+        let messagerTemplate = '';
+        
+        messagerTemplate = ` 
+        <div class="enter">
+            <div class="status">
+                <p>(${message[i].time})<strong> ${message[i].from}</strong> ${message[i].text}</p>
+            </div>
+        </div>`;
+        container.innerHTML += messagerTemplate;
+
+        if (message[i].type === 'message') {
+            messager();
+        }	
+
+        if (message[i].type === 'private_message') {
+            privateMessage();
+        }	
+    }
+}
+
+function messager() {
+    const status = document.querySelector('.enter');
+    status.classList.add('message');
+    status.classList.remove('status');
+}
+
+function privateMessage() {
+    const status = document.querySelector('.enter');
+    status.classList.add('private-message');
+}
+
+//Entrar no bate-papo com o usuário
+let name;
+
+function choiceUserName() {
+    const userName = prompt('Digite seu nome:');
+
+    if (userName === undefined || userName === null) {
+        choiceUserName();
+    } else {
+        user = {
+            name: userName,
+        }
+
+        axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', user);
+        /* promise.catch(processError); */
+
+    }
+}
+
+/* let error;
+
+function processError() {
+
+    error = erro.response.status;
+
+    if (error === 400) {
+        alert('Já existe usuário com este nome. Escolha outro nome.');
+        choiceUserName();
+    }
+
+    if (error === 422) {
+        alert('Preencha o campo');
+        choiceUserName();
+    }
+} */
